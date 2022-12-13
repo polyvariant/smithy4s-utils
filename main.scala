9e9ad97974a382944f7b6bc5d6f53e8659349751
Jakub Kozłowski <kubukoz@gmail.com>
2022-12-14 00:28:45 +0100

    set name

--- main.scala
+++ main.scala
@@ -3,6 +3,7 @@
 //> using scala "2.13.10"
 //> using publish.repository "central-s01"
 //> using publish.organization "org.polyvariant"
+//> using publish.name "smithy4s-utils"
 package smithy4sutils.cache
 
 import smithy4s.kinds._
