e2067eecf41e717c2e981f483eea7e3436d73769
Jakub Kozłowski <kubukoz@gmail.com>
2022-12-14 00:45:51 +0100

    add meta

--- main.scala
+++ main.scala
@@ -4,5 +4,8 @@
 //> using publish.name "smithy4s-utils"
 //> using publish.user "env:SONATYPE_USERNAME"
 //> using publish.password "env:SONATYPE_PASSWORD"
+//> using publish.url "https://github.com/polyvariant/smithy4s-utils"
+//> using publish.license "Apache-2.0"
+//> using publish.developer "kubukoz|Jakub Kozłowski|https://github.com/kubukoz"
 package smithy4sutils.cache
 object demo {}
