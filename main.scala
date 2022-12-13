4459e1dd2b7785b3442dcfd1f651f72224dee3cd
Jakub Kozłowski <kubukoz@gmail.com>
2022-12-14 00:33:27 +0100

    eh

--- main.scala
+++ main.scala
@@ -2,5 +2,7 @@
 //> using publish.repository "central-s01"
 //> using publish.organization "org.polyvariant"
 //> using publish.name "smithy4s-utils"
+//> using publish.user "env:SONATYPE_USERNAME"
+//> using publish.password "env:SONATYPE_PASSWORD"
 package smithy4sutils.cache
 object demo {}
