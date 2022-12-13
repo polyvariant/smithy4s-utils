cdea75fed7e9a50a913c8f0189216d23da4ea743
Jakub Kozłowski <kubukoz@gmail.com>
2022-12-14 00:46:56 +0100

    set publish.scm

--- main.scala
+++ main.scala
@@ -5,6 +5,7 @@
 //> using publish.user "env:SONATYPE_USERNAME"
 //> using publish.password "env:SONATYPE_PASSWORD"
 //> using publish.url "https://github.com/polyvariant/smithy4s-utils"
+//> using publish.scm "https://github.com/polyvariant/smithy4s-utils"
 //> using publish.license "Apache-2.0"
 //> using publish.developer "kubukoz|Jakub Kozłowski|https://github.com/kubukoz"
 package smithy4sutils.cache
