3302131071a47faf71780e526d2938c83eca85df
Jakub Kozłowski <kubukoz@gmail.com>
2022-12-14 01:19:30 +0100

    figure out config

--- config.scala
+++ config.scala
@@ -1,11 +1,8 @@
 //> using publish.repository "central-s01"
 //> using publish.organization "org.polyvariant"
+//> using publish.computeVersion "git:tag"
 //> using publish.name "smithy4s-utils"
-//> using publish.user "env:SONATYPE_USERNAME"
-//> using publish.password "env:SONATYPE_PASSWORD"
 //> using publish.url "https://github.com/polyvariant/smithy4s-utils"
 //> using publish.scm "https://github.com/polyvariant/smithy4s-utils"
 //> using publish.license "Apache-2.0"
 //> using publish.developer "kubukoz|Jakub Kozłowski|https://github.com/kubukoz"
-package smithy4sutils.cache
-object demo {}
